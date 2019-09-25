5ea493f42a5f322931840c0af273c687c67fe79e
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2019-09-25 23:05:18 +0200

    add script to generate and compress data + statistics

---
+++ code/compressDevOpsData.ps1
@@ -0,0 +1,15 @@
+$compressionMethods = "gorilla", "deflate", "lz4", "zstandard"
+$compressionTimeframes = 2, 4, 8, 16, 48
+
+Clear-Content statistics.txt
+Add-Content statistics.txt "host_id,initial_size,compressed_size,compression_ratio,compression_method,timeframe"
+foreach ($method in $compressionMethods) {
+    foreach ($timeframe in $compressionTimeframes) {
+        for ($i = 0; $i -lt 50; $i++) {
+            Write-Host "Compressing file devops_${timeframe}h_60s_${i}.txt with compression method ${method}"
+            $statistics = Invoke-Expression "Get-Content ..\data\devops_$i.txt | Out-String -Stream | java -jar .\TimeseriesCompressionBenchmarks\build\libs\TimeseriesCompressionBenchmarks-all.jar $method"
+            $number_of_data_points = $timeframe * 60
+            Add-Content statistics.txt "${i},${statistics},${method},${timeframe},${number_of_data_points}"
+        }
+    }    
+}
--- code/generateData.ps1
+++ code/generateData.ps1
@@ -1,4 +1,21 @@
-for ($i = 0; $i -lt 10; $i++) {
-    Invoke-Expression "tsbs_generate_data -use-case=`"devops`" -seed=$i -scale=10 -timestamp-start=`"2019-01-01T00:00:00Z`" -timestamp-end=`"2019-01-01T02:00:00Z`" -log-interval=`"60s`" -format=`"cassandra`" > ..\data\devops_$i.txt"
+for ($i = 0; $i -lt 50; $i++) {
+    Invoke-Expression "tsbs_generate_data -use-case=`"devops`" -seed=$i -scale=1 -timestamp-start=`"2019-01-01T00:00:00Z`" -timestamp-end=`"2019-01-01T02:00:00Z`" -log-interval=`"60s`" -format=`"cassandra`" > ..\data\devops_2h_60s_$i.txt"
 }
+
+for ($i = 0; $i -lt 50; $i++) {
+    Invoke-Expression "tsbs_generate_data -use-case=`"devops`" -seed=$i -scale=1 -timestamp-start=`"2019-01-01T00:00:00Z`" -timestamp-end=`"2019-01-01T04:00:00Z`" -log-interval=`"60s`" -format=`"cassandra`" > ..\data\devops_4h_60s_$i.txt"
+}
+
+for ($i = 0; $i -lt 50; $i++) {
+    Invoke-Expression "tsbs_generate_data -use-case=`"devops`" -seed=$i -scale=1 -timestamp-start=`"2019-01-01T00:00:00Z`" -timestamp-end=`"2019-01-01T08:00:00Z`" -log-interval=`"60s`" -format=`"cassandra`" > ..\data\devops_8h_60s_$i.txt"
+}
+
+for ($i = 0; $i -lt 50; $i++) {
+    Invoke-Expression "tsbs_generate_data -use-case=`"devops`" -seed=$i -scale=1 -timestamp-start=`"2019-01-01T00:00:00Z`" -timestamp-end=`"2019-01-01T16:00:00Z`" -log-interval=`"60s`" -format=`"cassandra`" > ..\data\devops_16h_60s_$i.txt"
+}
+
+for ($i = 0; $i -lt 50; $i++) {
+    Invoke-Expression "tsbs_generate_data -use-case=`"devops`" -seed=$i -scale=1 -timestamp-start=`"2019-01-01T00:00:00Z`" -timestamp-end=`"2019-01-03T00:00:00Z`" -log-interval=`"60s`" -format=`"cassandra`" > ..\data\devops_48h_60s_$i.txt"
+}
+
 Exit
