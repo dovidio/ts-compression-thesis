892f233ed59cc19abb57a21474a6640e963204b0
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2019-09-27 23:36:10 +0200

    initial data analysis with devops data

--- code/compressDevOpsData.ps1
+++ code/compressDevOpsData.ps1
@@ -1,8 +1,9 @@
 $compressionMethods = "gorilla", "deflate", "lz4", "zstandard"
 $compressionTimeframes = 2, 4, 8, 16, 48
 
-Clear-Content statistics.txt
-Add-Content statistics.txt "host_id,initial_size,compressed_size,compression_ratio,compression_method,timeframe"
+$fileName = "devops_statistics.txt"
+Clear-Content $fileName
+Add-Content $fileName "host_id,initial_size,compressed_size,compression_ratio,compression_method,timeframe,datapoints_number"
 foreach ($method in $compressionMethods) {
     foreach ($timeframe in $compressionTimeframes) {
         for ($i = 0; $i -lt 50; $i++) {
@@ -10,7 +11,7 @@ foreach ($method in $compressionMethods) {
             $getContent = "Get-Content ..\data\devops_${timeframe}h_60s_${i}.txt"
             $statistics = Invoke-Expression "$getContent | Out-String -Stream | java -jar .\TimeseriesCompressionBenchmarks\build\libs\TimeseriesCompressionBenchmarks-all.jar $method"
             $number_of_data_points = $timeframe * 60
-            Add-Content statistics.txt "${i},${statistics},${method},${timeframe},${number_of_data_points}"
+            Add-Content $fileName "${i},${statistics},${method},${timeframe},${number_of_data_points}"
         }
     }    
 }
