f9d3ffefb91fb468f587df99caecf798626b2fcc
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2019-10-11 14:08:07 +0200

    add taxi data analysis

--- code/taxi_data_analysis.py
+++ code/taxi_data_analysis.py
@@ -2,24 +2,20 @@ import numpy as np
 import pandas as pd
 from matplotlib import rcParams
 
-
 # import dataset
 # contains compression performance data of 4 different compression algorithms:
 # deflate, gorilla, lz4 and zstandard
-# for different metrics
-df = pd.read_csv('../data/dynatrace_statistics.csv')
-df.head()
+# for taxi data
+df = pd.read_csv('../data/taxi_statistics.csv')
 # check that we have the same amount of observations for each algorithm
 df['compression_method'].value_counts()
 
 # group by compression method and timeframe and get means
-groupedMeans = df.groupby(['metric', 'compression_method']).mean()['compression_ratio'].unstack()
+groupedMeans = df.groupby(['compression_method']).mean()['compression_ratio']
 rcParams.update({'figure.autolayout': True})
 plt = groupedMeans.plot.bar()
-plt.legend(bbox_to_anchor=(1.2, 0.5))
 plt.set_ylabel('Compression ratio')
-plt.set_xlabel('Metrics')
-plt.set_xticklabels(['CPU I/O Wait', 'CPU System', 'CPU Usage', 'CPU User', 'Disk Read Time', 'Disk Write Time', 'Memory Available', 'Memory Used'])
-plt.legend()
+plt.set_xlabel('Compression algorithm')
+
 figure = plt.get_figure()
-figure.savefig('../thesis/figure/dynatrace_bar_chart.png')
+figure.savefig('../thesis/figure/taxi_bar_chart.png')
